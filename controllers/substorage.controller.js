dd8fd46f38a16249b552acc9cae30c6419ece2c2
Shriram Mishra <ram.techcare27@gmail.com>
2022-05-06 23:39:25 +0530

    update items in storage

--- controllers/substorage.controller.js
+++ controllers/substorage.controller.js
@@ -25,7 +25,7 @@ exports.additems = async(request, response) => {
         charges: request.body.charges,
         description: request.body.description
     }
-    let storage = await substorageM.findOne({ _id: request.body.id });
+    let storage = await substorageM.findOne({ _id: request.body.storage_id });
     console.log(storage);
     storage.items.push(item);
     storage.save().then(result => {
@@ -36,7 +36,60 @@ exports.additems = async(request, response) => {
         })
 }
 
+// exports.viewItems = async(request, response) => {
+//     var storage = await substorageM.findOne({ _id: request.params.id });
+//     console.log(storage)
+//         storage.find(items)
+//     let res = storage.items.map(({ name }) => name).then(result => {
+//         console.log(res);
+//         console.log(name);
+//         console.log(result);
+//         return response.status(200).json(result)
+//     }).catch(error => {
+//         return response.status(500).json(error)
+//     })
+// }
 
+exports.updateitems = async(request, response) => {
+    var storage = await substorageM.findOne({ _id: request.params.id });
+    var result = storage.items.filter(obj => {
+        return obj._id == (request.body.id)
+    })
+    const { charges, name, description } = request.body;
+    if (name) {
+        result[0].name = name
+    }
+    if (charges) {
+        result[0].charges = charges
+    }
+    if (description) {
+        result[0].description = description
+    }
+    for (let i = 0; i < storage.items.length; i++) {
+        if (storage.items[i]._id == request.body.id) {
+            storage.items.pull({ _id: request.body.id });
+            storage.items.push({ name: result[0].name, charges: result[0].charges, description: result[0].description, _id: result[0]._id })
+        }
+    }
+    storage.save().then((result) => {
+        response.status(200).json(result)
+    }).catch((err) => {
+        response.status(500).json(err)
+
+    })
+}
+
+exports.deleteitems = async(request, response) => {
+    let storage = await substorageM.findOne({ _id: request.params.id });
+    console.log(storage);
+    storage.items.pull({ _id: request.body.id });
+    storage.save().then(result => {
+        return response.status(201).json(result)
+    }).catch(
+        err => {
+            return response.status(500).json(err);
+        })
+}
 
 
 exports.view = (request, response) => {
