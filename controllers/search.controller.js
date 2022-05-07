ecb75085f6fbf62d164867b3bd4eeb530f5512ca
palaknigam03 <95473476+palaknigam03@users.noreply.github.com>
2022-05-07 17:37:26 +0530

    seraching location

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -3,12 +3,23 @@ const Contrat = require('../models/subStorage.model');
 const Substorage = require('../models/subStorage.model'); 
 
 
-exports.searchservice=(request,response)=>{
+exports.searchlocation=(request,response)=>{
     searchWord = request.body.word
-    // console.log(request.body)
+    //  console.log(request.body)
+    //  console.log(request.body.word)
+    Substorage.find({location: { $regex: '^' + searchWord, $options: '$i' } }).then(result=>{
+        //console.log(result)
+       response.status(200).json(result);
+    }).catch((err)=>{
+        response.status(500).json(err);
+    })
+}
+
+exports.seachservice=(request,response)=>{
+    searchWord = request.body.word
+     console.log(request.body)
      console.log(request.body.word)
-     Substorage.find({location : { $regex: '^' + searchWord, $options: '$i' } })
-     .then(result=>{
+    Machine.find({toolname: { $regex: '^' + searchWord, $options: '$i' } }).then(result=>{
         console.log(result)
        response.status(200).json(result);
     }).catch((err)=>{
